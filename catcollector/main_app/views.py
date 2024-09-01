24666f9653f33c563b3ea12be35ab5d912c0536d
AhmedAshlw <ahmedit5216@gmail.com>
2024-09-01 14:53:26 +0300

    C2

--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import redirect, render
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
 from .forms import FeedingForm  # type: ignore
 
 
@@ -23,22 +24,25 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    # instantiate FeedingForm to be rendered in the template
+
+    # Only get the toys the cat does not have
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in=cat.toys.all().values_list("id"))
+
     feeding_form = FeedingForm()
     return render(
         request,
         "cats/detail.html",
         {
-            # include the cat and feeding_form in the context
             "cat": cat,
             "feeding_form": feeding_form,
+            "toys": toys_cat_doesnt_have,  # send those toys
         },
     )
 
 
 class CatCreate(CreateView):
     model = Cat
-    fields = "__all__"
+    fields = ["name", "breed", "description", "age"]
     success_url = "/cats/"
 
 
@@ -63,4 +67,33 @@ def add_feeding(request, cat_id):
         new_feeding = form.save(commit=False)
         new_feeding.cat_id = cat_id
         new_feeding.save()
+    return redirect("cat-detail", cat_id=cat_id)
+
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = "__all__"
+
+
+class ToyList(ListView):
+    model = Toy
+
+
+class ToyDetail(DetailView):
+    model = Toy
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ["name", "color"]
+
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = "/toys/"
+
+
+def associate_toy(request, cat_id, toy_id):
+    # Note that you can pass a toy's id instead of the whole object
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
     return redirect("cat-detail", cat_id=cat_id)
